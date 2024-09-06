387e2481a59b125637739bd9632b4b9da0567139
AlejandroPaclibar <147416357+AlejandroPaclibar@users.noreply.github.com>
2024-09-06 08:16:06 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Alejandro A. Paclibar';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'A dedicated 4th-year student from the College of Arts Sciences and Technology, focused on learning and developing skills in building websites and web applications. Currently exploring various programming languages, frameworks, and development tools.';
+export const ABOUT_TEXT_2 =
+  'Learning on enhancing both frontend and backend development skills, while collaborating on projects that involve designing, implementing, and deploying functional solutions.';
+export const ABOUT_TEXT_3 =
+  'Eager to continue learning and expanding knowledge in web development, with a focus on gaining practical experience and building a strong foundation in modern web technologies.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  '4th-year student pursuing a degree at the College of Arts Science and Technology. Passionate about learning web development and currently gaining experience in frontend and backend technologies.';
+export const RESUME_TEXT_2 =
+  'Aspiring web developer with a focus on building websites and web applications, actively learning and growing skills in JavaScript and modern web frameworks.'
